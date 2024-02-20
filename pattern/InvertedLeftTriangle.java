526c60e7fa783299d889360d37b4b65f50f94d39
AbhishekWarad <abhishekwarad123@gmail.com>
2024-02-20 13:04:10 +0530

    add Inverted left trianle.java

---
+++ pattern/InvertedLeftTriangle.java
@@ -0,0 +1,17 @@
+package pattern;
+
+public class InvertedLeftTriangle {
+    public static void main(String[] args) {
+        int i,j;
+        for(i=1;i<=5;i++){
+            for(j=1;j<i;j++){
+                System.out.print("  ");
+            }
+            for(j=1;j<=6-i;j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+
+    }
+}
